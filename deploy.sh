4dab98b45af468b5a47407b092bd5edc07a5904e
dandu1008 <dandu.8x@gmail.com>
2019-03-19 17:52:52 +0000

    added travis & deploy yamls

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t dandu1008/jenkins-master:latest -t dandu1008/jenkins-master:$SHA -f ./master/Dockerfile ./master
+docker build -t dandu1008/jenkins-slave:latest -t dandu1008/jenkins-slave:$SHA -f ./slave/Dockerfile ./slave
+
+docker push dandu1008/jenkins-master:latest
+docker push dandu1008/jenkins-slave:latest
+
+docker push dandu1008/jenkins-master:$SHA
+docker push dandu1008/jenkins-slave:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/jenkins-master-deployment jenkins-master=dandu1008/jenkins-master:$SHA
+kubectl set image deployments/jenkins-slave-deployment jenkins-slave=dandu1008/jenkins-slave:$SHA
+
