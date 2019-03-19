fe12464e391c551cfa2afa64c21e2f892217b6c7
dandu1008 <dandu.8x@gmail.com>
2019-03-19 19:08:08 +0000

    updated issuer & certificate yamls

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker build -t dandu1008/jenkins-master:latest -t dandu1008/jenkins-master:$SHA -f ./master/Dockerfile ./master
 docker build -t dandu1008/jenkins-slave:latest -t dandu1008/jenkins-slave:$SHA -f ./slave/Dockerfile ./slave
 
@@ -11,9 +13,9 @@ kubectl apply -f k8s
 kubectl set image deployments/jenkins-master-deployment jenkins-master=dandu1008/jenkins-master:$SHA
 kubectl set image deployments/jenkins-slave-deployment jenkins-slave=dandu1008/jenkins-slave:$SHA
 
-if [[ -z "$DNS"]]
+if [[ -z "$DNS" ]]
 then
-  echo "No DNS is Configured"
+  echo "No DNS is Configured; Deployment is completed"
 else
   kubectl apply -f k8s_dns #DNS is not empty
 fi   
